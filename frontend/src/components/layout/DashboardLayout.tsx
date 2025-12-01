fceb14fc792836d25f0166abf194128f86e583ff
Aguti1902 <agutierrezgomez00@gmail.com>
2025-12-01 14:59:01 +0100

    fix: Asegurar que el menú inferior nunca se mueva con CSS !important

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -112,7 +112,7 @@ export default function DashboardLayout() {
       </main>
 
       {/* Bottom navigation */}
-      <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-800 z-40">
+      <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-800 z-50" style={{ position: 'fixed' }}>
         <div className="max-w-7xl mx-auto px-2">
           <div className="flex justify-around">
             {navItems.map((item) => {
